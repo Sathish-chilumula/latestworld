4bf7ab9344ab18355ca6dac7d166959e047f52ca
Sathish-chilumula <chilumulas62@gmail.com>
2026-03-20 20:31:01 +0530

    Delete app directory

--- app/robots.ts
+++
@@ -1,7 +0,0 @@
-export default function robots() {
-  return {
-    rules: [{ userAgent: '*', allow: '/' }],
-    sitemap: 'https://latestworld.in/sitemap.xml',
-    host: 'https://latestworld.in',
-  }
-}
